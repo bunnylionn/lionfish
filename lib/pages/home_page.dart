eb8bcc1b089235aab9107e17262a1bc3a9294683
zulai <zulaikhaekaa27@gmail.com>
2024-12-20 13:54:34 +0800

    homepage update ika n jannah

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:eatbytes/components/my_drawer.dart';
+import 'package:eatbytes/components/my_sliver_app_bar.dart';
 import 'package:flutter/material.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,17 +10,28 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+//ika & jannah
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //AppBar
       appBar: AppBar(
       title: Text("Home"),
       backgroundColor: Theme.of(context).colorScheme.surface,
       ),
+      
+      //sliver
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            child: Text('Hello'),
+            title: Text('title'),
+          ),
+        ],
+        body: Container(color: Colors.blue),
+      ),
     );
   }
 }
-
-// jannah punya
