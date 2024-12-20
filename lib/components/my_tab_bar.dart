d83e56602a5cd606309a790f45dc60359d6e9b4e
Amalin <nramalinn19@gmail.com>
2024-12-21 00:56:11 +0800

    Add custom tab bar component

---
+++ lib/components/my_tab_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+class MyTabBar extends StatelessWidget {
+  final TabController tabController;
+
+  const MyTabBar ({
+    super.key,
+    required this.controller,
+
+   });
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Container(
+      child: TabBar(
+        controller: tabController,
+        tabs: [
+          //1st tab
+          Tab(
+            icon: Icon (Icons.home),
+          ),
+        
+          //2nd tab
+          Tab(
+            icon: Icon(Icons.settings),
+          ),
+        ], 
+      ),
+    );
+  }
+}
